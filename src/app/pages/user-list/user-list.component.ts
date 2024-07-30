fcaf2caeeb303e3ec70e4bd876d05c451bde5b6e
Cachorro <fj_alvarez@yahoo.com>
2024-07-30 13:03:07 +0200

    Hecha vista home con todos los usuarios

--- src/app/pages/user-list/user-list.component.ts
+++ src/app/pages/user-list/user-list.component.ts
@@ -2,11 +2,12 @@ import { Component, inject, OnInit } from '@angular/core';
 import { UsersServiceService } from '../../services/users-service.service';
 import { Iuser } from '../../interfaces/iuser.interface';
 import { IuserPage } from '../../interfaces/iuser-page.interface';
+import { UserCardComponent } from '../../components/user-card/user-card.component';
 
 @Component({
   selector: 'app-user-list',
   standalone: true,
-  imports: [],
+  imports: [UserCardComponent],
   templateUrl: './user-list.component.html',
   styleUrl: './user-list.component.css',
 })
@@ -18,7 +19,7 @@ export class UserListComponent implements OnInit {
     try {
       let pagina: IuserPage = await this.usuariosServices.getAll();
       this.misUsuarios = pagina.results;
-      console.table(this.misUsuarios);
+      console.log(this.misUsuarios);
     } catch (error) {
       console.log(error);
     }
